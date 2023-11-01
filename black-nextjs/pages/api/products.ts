c492a35146b9718cd4a4348a1ad6402717890bce
henryemiliano <134318580+henryemiliano@users.noreply.github.com>
2023-10-31 21:00:08 -0300

    Delete black-nextjs directory

--- black-nextjs/pages/api/products.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import products from "../../database.json";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(products);
-}
